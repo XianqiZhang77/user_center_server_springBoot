89e1e5f038f18e0e53fccbeab86b6065d737c9aa
Xianqi Zhang <zhangxianqica@gmail.com>
2021-07-12 15:37:17 -0400

    Spring Data Jpa interface

---
+++ src/main/java/com/concordia/dao/RegisterRecordDao.java
@@ -0,0 +1,11 @@
+package com.concordia.dao;
+
+import com.concordia.pojo.RegisterRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegisterRecordDao extends JpaRepository<RegisterRecord, String> {
+
+
+}
---
+++ src/main/java/com/concordia/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.concordia.dao;
+
+import com.concordia.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends JpaRepository<User, String> {
+
+
+
+}
