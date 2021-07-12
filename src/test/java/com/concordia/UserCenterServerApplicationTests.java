3fb912eb95f65af929bd091026c71c5073ee793f
Xianqi Zhang <zhangxianqica@gmail.com>
2021-07-11 23:07:42 -0400

    springboot init, dependencies

---
+++ src/test/java/com/concordia/UserCenterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concordia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCenterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
